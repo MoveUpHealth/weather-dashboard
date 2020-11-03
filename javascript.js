c862d23dea697f9001df9ddb9081eba6584791fc
Jessica Tax <69859383+MoveUpHealth@users.noreply.github.com>
2020-11-03 09:12:53 -0800

    coded for 5-day forecast call and display info

--- javascript.js
+++ javascript.js
@@ -3,6 +3,10 @@ var date = new Date();
 var year = date.getFullYear();
 var month = date.getMonth()+1;
 var currentDate = date.getDate();
+var forecastDate = []
+var forecastIcon = []
+var forecastTemp = []
+var forecastHumidity = []
 
 if (currentDate < 10) {
   currentDate = '0' + currentDate;
@@ -11,8 +15,8 @@ if (month < 10) {
   month = '0' + month;
 }
 
-console.log(month+'/' + currentDate + '/'+ year);
 
+//Search button click event...
 $(".btn-search").on("click", function(e) {
     e.preventDefault()
     $('.form-control').empty()
@@ -41,6 +45,7 @@ $.ajax({
     $("#currentHumidity").text("Humidity: " + humidity + "%")
     $("currentWind").text("Wind Speed: " + speed)
 
+    // to get UV Index...
     var uvUrl = 'http://api.openweathermap.org/data/2.5/uvi?lat=' + latitude + '&lon=' + longitude + '&appid=' + apiKey
 
     $.ajax({
@@ -52,7 +57,72 @@ $.ajax({
         $("#currentUV").text("UV Index: " + uvIndex)
     })
 
+    var forecastUrl = 'https://api.openweathermap.org/data/2.5/forecast?q=' + cityName + '&units=imperial&appid=' + apiKey
+
+    $.ajax({
+        url: forecastUrl,
+        method: "GET"
+    }).then(function(cast){
+        console.log(cast.list)
+            for(i = 1; i< cast.list.length; i ++){
+
+            if((i % 8) == 0){
+                console.log(i)
+            var castDate = cast.list[i].dt_txt
+            var futureDate = castDate.split(" ")[0]
+            var splitDate = futureDate.split('-')
+            var castYear = splitDate[0]
+            var castMonth = splitDate[1]
+            var castDay = splitDate[2]
+            var concatDate = castMonth + '/' + castDay + '/' + castYear
+            var castIconId = cast.list[i].weather[0].icon
+            var castTemp = cast.list[i].main.temp
+            var castHumidity = cast.list[i].main.humidity
+            
+            forecastDate.push(concatDate)
+            forecastIcon.push(castIconId) 
+            forecastTemp.push(castTemp)
+            forecastHumidity.push(castHumidity)
+            
+            } else if(i == 39) {
+                
+            
+            var castDate = cast.list[i].dt_txt
+            var futureDate = castDate.split(" ")[0]
+            var splitDate = futureDate.split('-')
+            var castYear = splitDate[0]
+            var castMonth = splitDate[1]
+            var castDay = splitDate[2]
+            var concatDate = castMonth + '/' + castDay + '/' + castYear
+            var castIconId = cast.list[i].weather[0].icon
+            var castTemp = cast.list[i].main.temp
+            var castHumidity = cast.list[i].main.humidity
+            
+            forecastDate.push(concatDate)
+            forecastIcon.push(castIconId) 
+            forecastTemp.push(castTemp)
+            forecastHumidity.push(castHumidity)
+    }
+    }
+    var dateElement = document.getElementsByClassName('date')
+    var iconElement = document.getElementsByClassName('icon')
+    var tempElement = document.getElementsByClassName('temperature')
+    var humElement = document.getElementsByClassName('humidity')
+    for(var x = 0; x < dateElement.length; x++){
+        console.log(x)
+    var forecastIUrl = "http://openweathermap.org/img/w/" + forecastIcon[x] + ".png"
+
+    dateElement[x].innerHTML = forecastDate[x]
+    iconElement[x].setAttribute('src', forecastIUrl)
+    tempElement[x].innerHTML = "Temp: " + forecastTemp[x]
+    humElement[x].innerHTML = "Humidity: " + forecastHumidity[x] + '%'
+
+
+     } 
+    })
+
 
 
 })
 })
+
